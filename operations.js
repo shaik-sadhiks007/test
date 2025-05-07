84a8ce37fe9cb2584d2e96d10be66f53f3ce6169
shaik-sadhiks007 <shaik.sadhiks007@gmail.com>
2025-05-07 21:19:26 +0530

    operations in js

---
+++ operations.js
@@ -0,0 +1,108 @@
+// console.log("this is manual js code")
+
+
+// arthemetic operators , +,-,*,/,%
+
+// assaignment operators ----- = , +=, -=,/=, *=
+
+
+let a = 5; 
+
+
+//    a = a + 2
+
+
+ a += 2
+
+
+console.log(a,"a")
+
+
+// comparasion operators ----- >, < , ==, ===, >=, <=, !=
+
+
+const b = 2 
+const c = "2"
+
+console.log (b, c, typeof(b), typeof(c), "values")
+
+
+console.log( b >= c, "comparision" )
+
+
+console.log( b == c, "equals, when u r using the ==, it will compare the value only")
+
+
+console.log(b === c , "strict equals, cmp value and data type")
+
+
+// not --- true , !true ---- false
+
+
+console.log(!true,"boolean not operation")
+
+
+// = , ==, ===
+
+console.log(b !== c,"comparing using not ")
+
+
+// -------------------------
+
+
+// logical operators ----- and , or , not
+
+
+console.log( false && false , "and operation")
+
+
+console.log( false ||  false," or operation")
+
+
+
+// -----------------------------------
+
+
+// conditions statements ---- if, else, else if
+
+
+const age = 18 ;
+
+// syntax
+
+
+// if(condition) {
+
+//     adfadfkadlfasdlfasldk
+
+// }
+
+
+// if (true) {
+
+// }
+
+
+if (age > 18  ||  age == 18 ) {
+    console.log("you are eligible to vote")
+} 
+else {
+
+    console.log("your are not eligible to vote")
+
+}
+
+console.log('end of the javascript')
+
+
+
+
+
+
+
+
+
+
+
+
+
