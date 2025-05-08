a7d92ddc7a610241f06d9d317443eb0d7760ae40
shaik-sadhiks007 <shaik.sadhiks007@gmail.com>
2025-05-08 20:54:21 +0530

    hoisting concept

---
+++ propertiesAndFunctions.js
@@ -0,0 +1,25 @@
+
+
+// strings ----- 
+
+
+let str1 = "sadhik";
+
+// properties and functions(methods)
+
+let c = a + 3 ; // imaginary numbers
+
+
+let z ; // undefined
+
+
+console.log(c,"c output")
+
+
+let a = 5;
+
+
+let d = a + 3;
+
+
+console.log(d,'d output')
